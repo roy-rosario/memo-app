13aff8ed73b3565e1faf1426a774f351555ed709
Roy Rosario <roy.rosario88@gmail.com>
2021-12-21 09:50:30 -0500

    routine update

--- src/pages/Dashboard/components/StatusBar.js
+++ src/pages/Dashboard/components/StatusBar.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import {StatusContainer, TextEditor, StanButton} from '../styles/dashboardStyles'
+import {StatusContainer, TextEditor, StanButton, EditCover} from '../styles/dashboardStyles'
 import { archiveDoc } from '../../../services/dataServices'
 
 export default function StatusBar({theme, tasks, change, info, mode_, write_back, cancel_}){
     return(
             <StatusContainer theme={theme}>
-                {mode_?
-                <TextEditor theme={theme}>
-                    <textarea value={info} maxLength="50" onChange={e => (change(e.target.value))}/>
-                    <StanButton theme={theme} onClick={write_back}>submit</StanButton>
-                    <StanButton theme={theme} onClick={()=>{cancel_(false)}}>cancel</StanButton>
-                </TextEditor>
-                :
+                
+                    {mode_ && <TextEditor theme={theme}>
+                        <textarea value={info} maxLength="50" onChange={e => (change(e.target.value))}/>
+                        <div>
+                            <StanButton theme={theme} onClick={write_back}>save</StanButton>
+                            <StanButton theme={theme} onClick={()=>{cancel_(false)}}>cancel</StanButton>
+                        </div>
+                    </TextEditor>
+                    }
                 <>
                     <h3>Status</h3>
                     <h4>Total Tasks: </h4>
@@ -19,7 +21,7 @@ export default function StatusBar({theme, tasks, change, info, mode_, write_back
                     {tasks.length}
                     </span>
                 </>
-                }
+                
             </StatusContainer>
     )
 }
