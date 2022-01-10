b1bf8ca24afbcba67af30bae5d465c7586bd0efa
Roy Rosario <roy.rosario88@gmail.com>
2022-01-09 22:54:52 -0500

    routine commit

---
+++ src/pages/Dashboard/components/TextEditorComponent.js
@@ -0,0 +1,20 @@
+import React, { useState, useContext} from 'react'
+import {StatusContainer, TextEditor, StanButton, EditCover, TextEditorTitle, TextEditorBody, TaskEntry} from '../styles/dashboardStyles'
+import { archiveDoc } from '../../../services/dataServices'
+import {EntryBodyContext} from '../../../utils/entryBodyContext' 
+
+export default  function TextEditorComponent({data}){
+    const [body, setBody] = useState('')
+    const {entryBody, setEntryBody} = useContext(EntryBodyContext)
+    return(
+        
+            <TextEditor theme={data.theme} display_={data.matchResult}>
+                <TextEditorTitle theme={data.theme} value={data.initialAdd? data.task : data.taskTitle} maxLength="50" onChange={e => (data.initialAdd? data.setTask(e.target.value) : data.setTaskTitle(e.target.value))}/>
+                <TextEditorBody theme={data.theme} value={data.initialAdd? body : entryBody} maxLength="250" onChange={e => (data.initialAdd? setBody(e.target.value) : setEntryBody(e.target.value))}/>
+                <div>
+                    <StanButton theme={data.theme} onClick={()=>{data.initialAdd? data.addTask(body, setBody) : data.editTask(entryBody)}}>save</StanButton>
+                    <StanButton theme={data.theme} onClick={data.cancel}>cancel</StanButton>
+                </div>
+            </TextEditor>     
+    )
+}
